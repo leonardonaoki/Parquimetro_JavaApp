df4d1af62ca057d6a15f1d298fd9efc67a001e96
Leonardo Naoki Higa <leonardonaoki@gmail.com>
2024-09-15 17:57:07 -0300

    Commit Inicial com a criação dos arquivos

---
+++ README.md
@@ -0,0 +1,2 @@
+# JAVA - Parquimetro App
+API de referência para um sistema de parquímetro, utilizando a tecnologia JAVA + SpringBoot.
---
+++ src/main/java/com/parquimetro/app/ParquimetroApp/ParquimetroAppApplication.java
@@ -0,0 +1,13 @@
+package com.parquimetro.app.ParquimetroApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParquimetroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParquimetroAppApplication.class, args);
+	}
+
+}
